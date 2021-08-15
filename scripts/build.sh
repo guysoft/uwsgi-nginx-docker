db498b8817f7d4da1e424cf75aad103e5e3708b5
Guy Sheffer <guysoft@gmail.com>
2021-08-15 15:49:06 +0300

    Fix typo

--- scripts/build.sh
+++ scripts/build.sh
@@ -9,4 +9,4 @@ if [ "$NAME" == "latest" ] ; then
     DOCKERFILE="python3.8"
 fi
 
-docker buildx build --platforms linux/amd64,linux/arm64,linux/arm/v7 -t "$use_tag" --file "./docker-images/${DOCKERFILE}.dockerfile" "./docker-images/"
+docker buildx build --platform linux/amd64,linux/arm64,linux/arm/v7 -t "$use_tag" --file "./docker-images/${DOCKERFILE}.dockerfile" "./docker-images/"
